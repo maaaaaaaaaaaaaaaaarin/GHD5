2087e8833dd400121413f5e2578c78bbab5cff56
maaaaaaaaaaaaaaaaarin <marin.ffxiv@gmail.com>
2019-12-11 21:18:45 +0100

    Add tsconfig.json for esModuleInterop to be set to true

---
+++ poster.ts
@@ -0,0 +1,20 @@
+import request from 'request';
+
+const req = {
+    url:'http://localhost/webhook',
+    method:'POST',
+    json: {
+        "hello":"world",
+        "1+1":2,
+    }
+}
+
+request(req, (err, res, body) => {
+    console.log(`
+        
+        Server response:
+
+            ${body}
+
+    `);
+});
