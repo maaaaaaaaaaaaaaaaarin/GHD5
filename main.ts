43192c33226f09e8004ebe0d9949c93eeef7c73a
maaaaaaaaaaaaaaaaarin <marin.ffxiv@gmail.com>
2019-12-11 21:22:24 +0100

    Modify main to host server locally, accept POSTs on 80 and log back request contents

--- main.ts
+++ main.ts
@@ -1,13 +1,48 @@
-import * as imports from ".";
-// Exemple:
-// import { Cours } from imports;
+import {
+    Groupe,
+    EmploisDuTemps
+} from ".";
 
-import { Express } from 'express';
+import express, {Application}  from 'express';
 
-let main: Function;
+const app : Application = express();
+let   main: Function;
+let   port: number = 80;
+
+app.use(express.json());
+
+
+const SEMESTRE = function(semestre: number): any[][] {
+    let out: any[][] = [];
+    "ABCDE".split('').forEach(s => {
+        out.push([semestre, s])
+    });
+    return out;
+};
+
+
 (main = () : void => {
     
 
+    // Chargement des groupes;
 
+    let Groupes: Groupe[] = [];
+    SEMESTRE(3).forEach(g => {
+        Groupes.push(new Groupe(g[0], g[1]));
+    });
 
+    app.post('/webhook', (request, response) => {
+
+        console.log(request.body);
+        response.send("OK");
+
+    });
+
+    app.listen(port, () => {
+        console.log(`
+            
+            Listening on port ${port} ...
+
+        `);
+    })
 })();
